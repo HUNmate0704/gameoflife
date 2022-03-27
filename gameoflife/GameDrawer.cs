b4d44813587fbb59f32456460c06ca5b2ee016af
hunma <hunma@DARKER-PC>
2022-03-27 12:48:59 +0200

    Add project files.

---
+++ gameoflife/GameDrawer.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameoflife
+{
+    internal class GameDrawer
+    {
+        String[,] drawArray;
+        //A=alive, O=dead, 29 is max size, cause this is the default console height and width 
+        public GameDrawer(int row, int column)
+        {
+            if (row > 29)
+            {
+                throw new OutOfBoundsException("Row too big, max size: 29");
+            }
+
+            if (column > 120)
+            {
+                throw new OutOfBoundsException("Column too big, max size: 120");
+            }
+
+            drawArray = new string[row, column];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    drawArray[i, j] = "O";
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(0, Console.WindowTop);
+            for (int i = 0; i < drawArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < drawArray.GetLength(1); j++)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;     //dead color
+                    if (drawArray[i, j] == "A")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Magenta;  //alive color
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void NextGeneration()
+        {
+            String[,] nextStateDrawArray = drawArray.Clone() as String[,];
+
+
+            for (int i = 0; i < drawArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < drawArray.GetLength(1); j++)
+                {
+                    if (drawArray[i, j] == "A") //if alive
+                    {
+                        if (Neighbors(i, j) < 2)
+                        {
+                            int ne = Neighbors(i, j);
+                            nextStateDrawArray[i, j] = "O";
+
+                        }
+                        if (Neighbors(i, j) > 3)
+                        {
+                            nextStateDrawArray[i, j] = "O";
+                        }
+                    }
+                    else //if dead
+                    {
+                        if (Neighbors(i, j) == 3)
+                        {
+                            nextStateDrawArray[i, j] = "A";
+                        }
+                    }
+                }
+            }
+
+            //comment the line below if you want to separate the drawing from the next generation
+            Draw();
+
+            drawArray = nextStateDrawArray.Clone() as string[,];  //weird way, but needed to clone, "as" is mandatory
+        }
+        public int Neighbors(int row, int column)
+        {
+            int neighborCounter = 0;
+
+            if (row != 0 && column != 0)
+            {
+                if (drawArray[row - 1, column - 1] == "A") //left up
+                {
+                    neighborCounter++;
+                }
+            }
+            if (row != 0)
+            {
+                if (drawArray[row - 1, column] == "A") //left
+                {
+                    neighborCounter++;
+                }
+            }
+            if (row != 0 && column != 119)
+            {
+                if (drawArray[row - 1, column + 1] == "A") //right up
+                {
+                    neighborCounter++;
+                }
+            }
+            if (column != 119)
+            {
+                if (drawArray[row, column + 1] == "A") //right
+                {
+                    neighborCounter++;
+                }
+            }
+            if (row != 28 && column != 119)
+            {
+                if (drawArray[row + 1, column + 1] == "A") //right down
+                {
+                    neighborCounter++;
+                }
+            }
+            if (row != 28)
+            {
+                if (drawArray[row + 1, column] == "A") //down
+                {
+                    neighborCounter++;
+                }
+            }
+            if (row != 28 && column != 0)
+            {
+                if (drawArray[row + 1, column - 1] == "A") //left down
+                {
+                    neighborCounter++;
+                }
+            }
+            if (column != 0)
+            {
+                if (drawArray[row, column - 1] == "A") //left
+                {
+                    neighborCounter++;
+                }
+            }
+            return neighborCounter;
+        }
+
+        public void AddCell(int row, int column)
+        {
+            drawArray[row, column] = "A";
+        }
+    }
+}
---
+++ gameoflife/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameoflife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameDrawer gamedraw = new GameDrawer(29, 120); //max 29 row and 120 column
+
+            //crawls right down
+            gamedraw.AddCell(3, 2);
+            gamedraw.AddCell(4, 3);
+            gamedraw.AddCell(2, 4);
+            gamedraw.AddCell(3, 4);
+            gamedraw.AddCell(4, 4);
+            //3 in one line
+            //jatekrajzolo.AddCell(7, 5);
+            //jatekrajzolo.AddCell(7, 6);
+            //jatekrajzolo.AddCell(7, 7);
+
+            //3 in one column
+            //jatekrajzolo.AddCell(7, 3);
+            //jatekrajzolo.AddCell(8, 3);
+            //jatekrajzolo.AddCell(9, 3);
+
+            //cube
+            //jatekrajzolo.AddCell(7, 3);
+            //jatekrajzolo.AddCell(7, 4);
+            //jatekrajzolo.AddCell(8, 3);
+            //jatekrajzolo.AddCell(8, 4);
+
+            //works on keypress
+            //while (true)
+            //{
+            //    gamedraw.Draw();
+            //    Console.ReadKey();
+            //    gamedraw.NextGeneration();
+            //}
+
+
+            //works on tick
+            while (true)
+            {
+                gamedraw.Draw();
+                System.Threading.Thread.Sleep(100);
+                gamedraw.NextGeneration();
+            }
+
+        }
+    }
+}
